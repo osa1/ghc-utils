2d6dacab28fd2aea2b37e89054bc492d530c2504
Ömer Sinan Ağacan <omeragacan@gmail.com>
2020-03-11 15:26:40 +0300

    obj-loc: Fix a few bugs

--- bin/obj_loc.rs
+++ bin/obj_loc.rs
@@ -12,6 +12,11 @@ An example gdb script for this program:
     printf ">>> %p -> %p size: %d\n", from, to, size
     continue
     end
+    break Evac.c:148
+    commands 3
+    printf ">>> %p -> %p size: %d\n", from, to, size
+    continue
+    end
 
  */
 
@@ -37,7 +42,7 @@ impl fmt::Debug for Addr {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 struct AddrSize {
     addr: Addr,
     size: u64,
@@ -60,6 +65,14 @@ impl GC {
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
+struct Moves {
+    /// Object first appears in this era.
+    era: u64,
+    /// All the moves of the objects. First move is in `era`.
+    moves: Vec<Addr>,
+}
+
 fn main() {
     let args = App::new("obj-loc")
         .arg(
@@ -75,6 +88,11 @@ fn main() {
     let file = File::open(path).unwrap_or_else(|_| panic!("Unable to open file: {}", path));
     let reader = BufReader::new(file);
 
+    let gcs = parse(reader);
+    repl(&gcs);
+}
+
+fn parse<B: BufRead>(reader: B) -> Vec<GC> {
     let mut gcs: Vec<GC> = vec![];
     let mut current_gc: Option<GC> = None;
 
@@ -112,9 +130,7 @@ fn main() {
         gcs.push(gc);
     }
 
-    // println!("{:#?}", gcs);
-
-    repl(&gcs);
+    gcs
 }
 
 fn parse_hex(s: &str) -> Option<u64> {
@@ -134,7 +150,14 @@ fn repl(gcs: &[GC]) {
                     println!("Unable to parse address: {}", line);
                 }
                 Some(addr) => {
-                    print_locs(gcs, addr);
+                    for moves in find_moves(gcs, addr) {
+                        let mut era = moves.era;
+                        for move_ in moves.moves {
+                            println!("{}: {:#?}", era, move_);
+                            era += 1;
+                        }
+                        println!();
+                    }
                 }
             },
             Err(ReadlineError::Interrupted | ReadlineError::Eof) => {
@@ -150,23 +173,43 @@ fn repl(gcs: &[GC]) {
     }
 }
 
-/// Find all occurences of a given object and print all locations
-fn print_locs(gcs: &[GC], addr: u64) {
+fn find_moves(gcs: &[GC], addr: u64) -> Vec<Moves> {
+    let addr = Addr(addr);
+    let mut ret = vec![];
+
+    let mut skip_next_fwd = false;
     for (era, gc) in gcs.iter().enumerate() {
-        if let Some(prev_loc) = gc.moves_from.get(&Addr(addr)) {
-            print_locs_(gcs, era, prev_loc.addr);
+        if let Some(prev_addr) = gc.moves_from.get(&addr) {
+            ret.push(find_locs(gcs, era, prev_addr.addr));
+            skip_next_fwd = true;
+            continue;
+        }
+
+        if skip_next_fwd {
+            skip_next_fwd = false;
+            continue;
+        }
+
+        skip_next_fwd = false;
+
+        if gc.moves_to.get(&addr).is_some() {
+            ret.push(find_locs(gcs, era, addr));
         }
     }
+
+    ret
 }
 
-/// Print moves of the given object which was moved to its given location at the given era
-fn print_locs_(gcs: &[GC], era: usize, addr: Addr) {
+/// Find moves of the given object which was moved to its given location at the given era
+fn find_locs(gcs: &[GC], era: usize, addr: Addr) -> Moves {
+    // println!("find_locs: era={}, addr={:#?}", era, addr);
+
     //
     // Backwards search
     //
 
     let mut first_era = era;
-    let mut bwd_moves: Vec<(Addr, Addr)> = vec![];
+    let mut bwd_moves: Vec<Addr> = vec![];
     let mut current_loc = addr;
 
     for prev_era in (0..era).rev() {
@@ -176,7 +219,7 @@ fn print_locs_(gcs: &[GC], era: usize, addr: Addr) {
                 break;
             }
             Some(prev_addr) => {
-                bwd_moves.push((prev_addr.addr, current_loc));
+                bwd_moves.push(prev_addr.addr);
                 current_loc = prev_addr.addr;
                 first_era = prev_era;
             }
@@ -187,7 +230,7 @@ fn print_locs_(gcs: &[GC], era: usize, addr: Addr) {
     // Forwards search
     //
 
-    let mut fwd_moves: Vec<(Addr, Addr)> = vec![];
+    let mut fwd_moves: Vec<Addr> = vec![];
     let mut current_loc = addr;
 
     for gc in &gcs[era..] {
@@ -196,12 +239,22 @@ fn print_locs_(gcs: &[GC], era: usize, addr: Addr) {
                 break;
             }
             Some(next_addr) => {
-                fwd_moves.push((current_loc, next_addr.addr));
+                fwd_moves.push(next_addr.addr);
                 current_loc = next_addr.addr;
             }
         }
     }
 
+    bwd_moves.reverse();
+    bwd_moves.push(addr);
+    bwd_moves.extend_from_slice(&fwd_moves);
+
+    Moves {
+        era: first_era as u64,
+        moves: bwd_moves,
+    }
+
+    /*
     // Print locations
     for (i, bwd_move) in bwd_moves.iter().rev().enumerate() {
         println!("{}: {:#?} -> {:#?}", first_era + i, bwd_move.0, bwd_move.1);
@@ -210,4 +263,147 @@ fn print_locs_(gcs: &[GC], era: usize, addr: Addr) {
     for (i, fwd_move) in fwd_moves.iter().enumerate() {
         println!("{}: {:#?} -> {:#?}", era + i, fwd_move.0, fwd_move.1);
     }
+    */
+}
+
+#[test]
+fn parse_test() {
+    let input = "\
+        >>> GC 1\n\
+        >>> 0x123 -> 0x124 size: 1\n\
+    ";
+
+    let gcs = parse(input.as_bytes());
+    assert_eq!(gcs.len(), 1);
+    assert_eq!(
+        gcs[0].moves_to.get(&Addr(0x123)),
+        Some(&AddrSize {
+            addr: Addr(0x124),
+            size: 1
+        })
+    );
+    assert_eq!(
+        gcs[0].moves_from.get(&Addr(0x124)),
+        Some(&AddrSize {
+            addr: Addr(0x123),
+            size: 1
+        })
+    );
+
+    let input = "\
+        >>> GC 1\n\
+        >>> 0x123 -> 0x124 size: 1\n\
+        >>> GC 2\n\
+        >>> 0x124 -> 0x125 size: 2\n\
+        >>> 0x100 -> 0x101 size: 3\n\
+    ";
+
+    let gcs = parse(input.as_bytes());
+    assert_eq!(gcs.len(), 2);
+    assert_eq!(
+        gcs[1].moves_to.get(&Addr(0x124)),
+        Some(&AddrSize {
+            addr: Addr(0x125),
+            size: 2
+        })
+    );
+}
+
+#[test]
+fn find_moves_test() {
+    let input = "\
+        >>> GC 1\n\
+        >>> 0x123 -> 0x124 size: 1\n\
+        >>> GC 2\n\
+        >>> 0x124 -> 0x125 size: 2\n\
+        >>> 0x100 -> 0x101 size: 3\n\
+    ";
+
+    let gcs = parse(input.as_bytes());
+
+    //
+    // Test fwd search
+    //
+
+    assert_eq!(
+        find_moves(&gcs, 0x123),
+        vec![Moves {
+            era: 0,
+            moves: vec![Addr(0x123), Addr(0x124), Addr(0x125)],
+        }]
+    );
+
+    assert_eq!(
+        find_moves(&gcs, 0x100),
+        vec![Moves {
+            era: 1,
+            moves: vec![Addr(0x100), Addr(0x101)],
+        }]
+    );
+
+    //
+    // Test bwd search
+    //
+
+    assert_eq!(
+        find_moves(&gcs, 0x101),
+        vec![Moves {
+            era: 1,
+            moves: vec![Addr(0x100), Addr(0x101)],
+        }]
+    );
+
+    assert_eq!(
+        find_moves(&gcs, 0x125),
+        vec![Moves {
+            era: 0,
+            moves: vec![Addr(0x123), Addr(0x124), Addr(0x125)],
+        }]
+    );
+
+    assert_eq!(
+        find_moves(&gcs, 0x124),
+        vec![Moves {
+            era: 0,
+            moves: vec![Addr(0x123), Addr(0x124), Addr(0x125)],
+        }]
+    );
+}
+
+#[test]
+fn complicated_test() {
+    // An interesting case that can legitemately happen in compacting GC: We move x to y, and z to
+    // x, in the same GC. Make sure we handle this correctly.
+
+    let input = "\
+        >>> GC 1\n\
+        >>> 0x124 -> 0x125 size: 2\n\
+        >>> 0x125 -> 0x126 size: 2\n\
+    ";
+
+    let gcs = parse(input.as_bytes());
+
+    assert_eq!(
+        find_moves(&gcs, 0x124),
+        vec![Moves {
+            era: 0,
+            moves: vec![Addr(0x124), Addr(0x125)],
+        }]
+    );
+
+    assert_eq!(
+        find_moves(&gcs, 0x125),
+        vec![Moves {
+            era: 0,
+            moves: vec![Addr(0x124), Addr(0x125)],
+        }]
+    );
+
+    assert_eq!(
+        find_moves(&gcs, 0x126),
+        vec![Moves {
+            era: 0,
+            moves: vec![Addr(0x125), Addr(0x126)],
+        }]
+    );
 }
